6c22d6786b1aa8a822e16a1012f8d84b19c39e65
Naidu <UC192122@ten.thomsonreuters.com>
2016-02-10 11:01:21 -0600

    2/10/2016

--- js/Controllers/notesCntrl.js
+++ js/Controllers/notesCntrl.js
@@ -1,26 +1,26 @@
 var notesCntrl = angular.module('notesController',[]);
 
 notesCntrl.controller('BasicNotesCntrl', ['$scope', function($scope){
-    $scope.intro = "Swara is a Sanskrit word that means a note in the octave.The seven basic swaras of the scale ";
+    $scope.intro = "Swara is a Sanskrit word that means a note in the octave.The basic swaras of the scale ";
     $scope.swaras = [
         {
             'name': 'Shadja',
-            'Short name': 'Sa',
+            'shortName': 'Sa',
             'notion': 'S'
         },
         {
             'name': 'Shuddha Rishabha',
-            'Short name': 'Ri',
+            'shortName': 'Ri',
             'notion': 'R1'
         },
         {
             'name': 'Shuddha Gandhara',
-            'Short name': 'Ga',
+            'shortName': 'Ga',
             'notion': 'G1'
         },
         {
             'name': 'Shuddha Madhyama',
-            'Short name': 'Ma',
+            'shortName': 'Ma',
             'notion': 'M'
         }
     ];
